66c415106d7f0db201a9e7796764a4aa9f9139aa
avinash2050kumar <avinash2050kumar@gmail.com>
2022-08-18 14:28:39 +0400

    fix: apply prettier, fix some minor issue

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    'babel-plugin-styled-components',
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: [
-          '.ios.js',
-          '.android.js',
-          '.js',
-          '.ts',
-          '.tsx',
-          '.json',
-        ],
-      },
-    ],
-  ],
+	presets: ['module:metro-react-native-babel-preset'],
+	plugins: [
+		'babel-plugin-styled-components',
+		[
+			'module-resolver',
+			{
+				root: ['./src'],
+				extensions: [
+					'.ios.js',
+					'.android.js',
+					'.js',
+					'.ts',
+					'.tsx',
+					'.json',
+				],
+			},
+		],
+	],
 };
